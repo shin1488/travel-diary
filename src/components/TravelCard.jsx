a8b0bfa674546a3fbec9a358ef365184332aaa85
shin1488 <syc1488@naver.com>
2026-01-12 14:50:43 +0900

    TravelList 컴포넌트 완성 및 css 작성 완료

---
+++ src/components/TravelCard.jsx
@@ -0,0 +1,19 @@
+// const [form, setForm] = useState({
+//  name: '',
+//  country: '',
+//  city: '',
+//  date: '',
+//  image: '',
+//  rating: 5,
+//  memo: ''
+//  });
+
+
+function TravelCard({ travel, onEdit, onDelete }) {
+   return (
+    <>
+    </>
+   );
+}
+
+export default TravelCard;
